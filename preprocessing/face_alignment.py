7b1ec7ab942a57a8e72ac899b512704a80f20122
Jude Su <fortunatemaker2603@gmail.com>
2021-12-03 23:40:33 +0800

    add FaceAligner class. (#2)

---
+++ preprocessing/face_alignment.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+
+
+class FaceAligner:
+    def __init__(self, left_eye_position=(0.35, 0.35), image_size=(256, 256)):
+        pass
+
+    def align(self, image, rect):
+        pass
