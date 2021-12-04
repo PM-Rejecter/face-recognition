2239194dfc22cc8f0a667ed8e3950d00d2127796
Jude Su <fortunatemaker2603@gmail.com>
2021-12-04 10:45:27 +0800

    setting default params of FaceAligner. (#2)

--- preprocessing/face_alignment.py
+++ preprocessing/face_alignment.py
@@ -3,8 +3,10 @@ import cv2
 
 
 class FaceAligner:
-    def __init__(self, left_eye_position=(0.35, 0.35), image_size=(256, 256)):
-        pass
+    def __init__(self, landmarks_predictor, left_eye_position=(0.35, 0.35), image_size=(256, 256)):
+        self.landmarks_predictor = landmarks_predictor
+        self.left_eye_position = left_eye_position
+        self.width, self.height = image_size
 
     def align(self, image, rect):
         pass
