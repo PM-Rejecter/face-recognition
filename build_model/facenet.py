e7c397b680833416575e961c540102ace0e8a141
Jude <fortunatemaker2603@gmail.com>
2022-02-15 00:33:20 +0800

    reformat. (#16)

--- build_model/facenet.py
+++ build_model/facenet.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow.keras.layers import Dense, Flatten, Conv2D
 from tensorflow.keras import Model
 
+
 class NN1(Model):
     def __init__(self):
         super(NN1, self).__init__()
