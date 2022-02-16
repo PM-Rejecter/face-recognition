9df0ea7e33c94c4015c8b3eeb3f98f2c10380fc2
Jude <fortunatemaker2603@gmail.com>
2022-02-17 01:49:15 +0800

    add loss and optimizer. (#16)

--- build_model/train_model.py
+++ build_model/train_model.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import tensorflow as tf
+import tensorflow_addons as tfa
 
 from build_model.facenet import NN1
 
@@ -35,8 +36,20 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
     batch_size=batch_size
 )
 
+
+# rescale image
+def _normalize_img(img, label):
+    img = tf.cast(img, tf.float32) / 255.
+    return img, label
+
+
+train_ds = train_ds.map(_normalize_img)
+val_ds = val_ds.map(_normalize_img)
+
 # import model
 model = NN1()
 
 # setting optimizer and loss function
+loss = tfa.losses.TripletSemiHardLoss(margin=0.2)
+optimizer = tf.keras.optimizers.Adam()
 
