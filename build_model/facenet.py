f081e35c7f623e10170ac35cc9b17f4f44e5d05a
Jude <fortunatemaker2603@gmail.com>
2022-02-17 23:46:39 +0800

    adjust model. (#16)

--- build_model/facenet.py
+++ build_model/facenet.py
@@ -9,15 +9,15 @@ def _reduce_conv_norm_pool(reduced_filters: int, conv_filters: int, norm: bool, 
     model = Sequential(name=f"level{level}")
 
     # add 1x1 reduced layer
-    model.add(Conv2D(reduced_filters, 1, name=f"conv{level}a"))
+    model.add(Conv2D(reduced_filters, 1, padding='same', name=f"conv{level}a"))
     # add conv2d
-    model.add(Conv2D(conv_filters, 3, name=f"conv{level}", activation='relu'))
+    model.add(Conv2D(conv_filters, 3, padding='same', name=f"conv{level}", activation='relu'))
     # add batch normalization
     if norm:
         model.add(BatchNormalization())
     # add max pooling
     if pool:
-        model.add(MaxPooling2D((3, 3), 2, name=f"pool{level}"))
+        model.add(MaxPooling2D((3, 3), 2, padding='same', name=f"pool{level}"))
 
     return model
 
@@ -27,8 +27,8 @@ class NN1(Model):
         super(NN1, self).__init__()
 
         # Build layers
-        self.conv1 = Conv2D(64, 7, 2, name='conv1', activation='relu')
-        self.pool1 = MaxPooling2D(pool_size=(3, 3), strides=2, name="pool1")
+        self.conv1 = Conv2D(64, 7, 2, name='conv1', activation='relu', padding='same')
+        self.pool1 = MaxPooling2D(pool_size=(3, 3), strides=2, padding='same', name="pool1")
         self.rnorm1 = BatchNormalization(name="rnrom1")
 
         self.block2 = _reduce_conv_norm_pool(64, 192, norm=True, pool=True, level=2)
@@ -37,24 +37,24 @@ class NN1(Model):
         self.block5 = _reduce_conv_norm_pool(256, 256, norm=False, pool=False, level=5)
         self.block6 = _reduce_conv_norm_pool(256, 256, norm=False, pool=True, level=6)
 
-        self.flatten = Flatten()
-        self.fc1 = tfa.layers.Maxout(32 * 128)
-        self.fc2 = tfa.layers.Maxout(32 * 128)
-        self.fc3 = Dense(128, activity_regularizer=l2(0.01))
+        self.flatten = Flatten(name="flatten")
+        self.reshape = Dense(4096, name="reshape")
+        self.fc1 = tfa.layers.Maxout(32*128, name="fc1")
+        self.fc2 = tfa.layers.Maxout(32*128, name="fc2")
+        self.fc3 = Dense(128, activity_regularizer=l2(0.01), name="fc3")
 
     def call(self, inputs, training=None, mask=None):
-        model = Sequential([
-            self.conv1,
-            self.pool1,
-            self.rnorm1,
-            self.block2,
-            self.block3,
-            self.block4,
-            self.block5,
-            self.block6,
-            self.flatten,
-            self.fc1,
-            self.fc2,
-            self.fc3
-        ])
-        return model(inputs)
+        x = self.conv1(inputs)
+        x = self.pool1(x)
+        x = self.rnorm1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+        x = self.block6(x)
+        x = self.flatten(x)
+        x = self.reshape(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return x
--- build_model/train_model.py
+++ build_model/train_model.py
@@ -1,7 +1,6 @@
-from pathlib import Path
-
 import tensorflow as tf
 import tensorflow_addons as tfa
+from pathlib import Path
 
 from build_model.facenet import NN1
 
@@ -11,7 +10,7 @@ for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
 # 讀入資料集
-data_dir = Path('data')
+data_dir = Path('../data')
 
 # 資料參數
 batch_size = 32
@@ -50,6 +49,59 @@ val_ds = val_ds.map(_normalize_img)
 model = NN1()
 
 # setting optimizer and loss function
-loss = tfa.losses.TripletSemiHardLoss(margin=0.2)
+loss_object = tfa.losses.TripletSemiHardLoss(margin=0.2)
 optimizer = tf.keras.optimizers.Adam()
 
+# setting evaluate metrics
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+
+val_loss = tf.keras.metrics.Mean(name='val_loss')
+val_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='val_accuracy')
+
+
+# train data function
+@tf.function
+def train_step(images, labels):
+    with tf.GradientTape() as tape:
+        predictions = model(images, training=True)
+        loss = loss_object(labels, predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+    train_loss(loss)
+    train_accuracy(labels, predictions)
+
+
+@tf.function
+def test_step(images, labels):
+    predictions = model(images, training=False)
+    t_loss = loss_object(labels, predictions)
+
+    val_loss(t_loss)
+    val_accuracy(labels, predictions)
+
+
+# training
+EPOCHS = 5
+
+for epoch in range(EPOCHS):
+    # Reset the metrics at the start of the next epoch
+    train_loss.reset_states()
+    train_accuracy.reset_states()
+    val_loss.reset_states()
+    val_accuracy.reset_states()
+
+    for images, labels in train_ds:
+        train_step(images, labels)
+
+    for test_images, test_labels in val_ds:
+        test_step(test_images, test_labels)
+
+    print(
+        f'Epoch {epoch + 1}, '
+        f'Loss: {train_loss.result()}, '
+        f'Accuracy: {train_accuracy.result() * 100}, '
+        f'Valuation Loss: {val_loss.result()}, '
+        f'Valuation Accuracy: {val_accuracy.result() * 100}'
+    )
